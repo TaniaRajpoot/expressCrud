9fc8062bd6d8538009ea5130113e016517334554
imtahirabatool <btahira183@gmail.com>
2025-02-07 17:18:17 +0500

    get request fixed!

--- routes/users.js
+++ routes/users.js
@@ -2,27 +2,27 @@ import express from "express";
 
 const router = express.Router();
 
-
-
 const users = [
   {
-  "firstname": "john",
-  "lastname": "david",
-  "age": 25
-}
-]
-
+    firstname: "john",
+    lastname: "david",
+    age: 25,
+  },
+];
 
 // all routes here start with /users
-router.get('/' , (req,res) => {
-console.log(users);
+router.get("/", (req, res) => {
+  console.log(users);
 
-    res.send('Hello');
+  // res.send('Hello');
+  res.status(200).json({
+    status: true,
+    users,
+  });
 });
 
-router.post('/' , (req,res) => {
-
+router.post("/", (req, res) => {
+  
 });
-
 
 export default router;
