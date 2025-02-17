ec633a94b89dccb597a7d80d19768b473daab048
TaniaRajpoot <rajpoottania299@gmail.com>
2025-02-17 22:09:30 +0500

    post request issue

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,22 @@
 import express from "express";
+import { v4 as uuidv4 } from 'uuid';
+
 
 const router = express.Router();
 
+
+
 const users = [
   {
-    firstname: "john",
-    lastname: "david",
-    age: 25,
-  },
+  "firstname": "john",
+  "lastname": "david",
+  "age": 25
+},
+ {
+  "firstname": "john",
+  "lastname": "david",
+  "age": 25
+}
 ];
 
 // all routes here start with /users
@@ -15,14 +24,27 @@ router.get("/", (req, res) => {
   console.log(users);
 
   // res.send('Hello');
-  res.status(200).json({
-    status: true,
-    users,
-  });
+  // res.status(200).json({
+  //   status: true,
+  //   users,
+  // });
+  res.send(users);
 });
 
-router.post("/", (req, res) => {
-  
+// let users = [];
+
+router.post('/', (req, res) => {
+  const user = req.body;
+console.log("testing", user);
+  if (!user.firstname) {
+    return res.status(400).send("Firstname is required");
+  }
+
+  const newUser = { ...user, id: uuidv4() };
+  users.push(newUser);
+
+  res.send(`User with the name ${user.firstname} added to the database`);
 });
+
 
 export default router;
